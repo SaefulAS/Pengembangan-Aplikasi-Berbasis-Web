9af817c89965de8dcd2f52352e6994a230866f35
SaefulAS <58874828+SaefulAS@users.noreply.github.com>
2021-11-19 20:53:07 +0700

    Update head.php

--- guest/head.php
+++ guest/head.php
@@ -6,7 +6,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">PHP Poison</a>
+      <a class="navbar-brand" href="#">Perpustakaan SMK PGRI 1 Kota Sukabumi</a>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav"><!-- 
